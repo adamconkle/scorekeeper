6c7a205908e3a0fb8c2d698cfc06b8df53e82370
Adam <adam.conkle@gmail.com>
2024-12-31 17:46:42 -0800

    Update scorekeeperscript.js

--- scorekeeperscript.js
+++ scorekeeperscript.js
@@ -22,7 +22,7 @@ const scoreInput = document.getElementById(scoreId);
       
       playerForm.innerHTML = `
         <input type="text" id="name${playerCount}" placeholder="Player ${playerCount} Name">
-        <input type="number" id="score${playerCount}" placeholder="0" disabled>
+        <input type="number" id="score${playerCount}" placeholder="0">
         <button onclick="changeScore('score${playerCount}', 1)">+</button>
         <button onclick="changeScore('score${playerCount}', -1)">-</button>
         <input type="reset" value="Reset" onclick="resetScore('score${playerCount}')">
